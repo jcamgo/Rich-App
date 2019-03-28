851eed61de9aa7e7509123dd3db3add5bffad0d1
jonny <jonnycamargo96@gmail.com>
2019-03-27 23:16:23 -0600

    Initial Commit

---
+++ Rich App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rich App
+//
+//  Created by Jonathan Orellana on 3/27/19.
+//  Copyright © 2019 jonathan orellana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Rich AppTests/Rich_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Rich_AppTests.swift
+//  Rich AppTests
+//
+//  Created by Jonathan Orellana on 3/27/19.
+//  Copyright © 2019 jonathan orellana. All rights reserved.
+//
+
+import XCTest
+@testable import Rich_App
+
+class Rich_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
